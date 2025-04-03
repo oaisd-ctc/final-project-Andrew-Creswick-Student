2390c77b81fd78394dc5d89a3f37000bd209ee08
Andrew-Creswick-Student <ancr131@student.oaisd.org>
2025-04-03 12:28:50 -0400

    Added some basic path finding.

---
+++ Assets/Scripts/NavAgentTest.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class NavAgentTest : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            Ray movePosition = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(movePosition, out var hitInfo))
+            {
+                agent.SetDestination(hitInfo.point);
+            }
+        }
+    }
+}
