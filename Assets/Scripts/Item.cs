c75f423fc8ae3a1f61bb8cf228395cd5019ea267
Andrew-Creswick-Student <ancr131@student.oaisd.org>
2025-05-02 12:00:13 -0400

    Added some more items.

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 [RequireComponent(typeof(MouseDrag))]
+[RequireComponent(typeof(Rigidbody))]
 public class Item : MonoBehaviour
 {
     [SerializeField] private Rigidbody rigidbody;
